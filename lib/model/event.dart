d207500aabb6c5822bc5c9ad08fd2e3743768ab4
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-05-07 11:43:13 +0300

    added demo list of events

---
+++ lib/model/event.dart
@@ -0,0 +1,14 @@
+class Event {
+  String name;
+  String description;
+  String location;
+  String date;
+  String time;
+
+  Event(
+      {required this.name,
+      required this.description,
+      required this.location,
+      required this.date,
+      required this.time});
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:social_hive_client/model/event.dart';
+
+// demo list of events
+final List<Event> events = <Event>[
+  Event(
+    name: 'Dance Party',
+    description: 'Dance party with DJ',
+    location: 'Bucharest',
+    date: '2021-10-10',
+    time: '20:00',
+  ),
+  Event(
+    name: 'Ski',
+    description: 'Ski in the mountains',
+    location: 'Busteni',
+    date: '2021-12-10',
+    time: '08:00',
+  ),
+  Event(
+    name: 'Hiking',
+    description: 'Hiking in the mountains',
+    location: 'Bucegi',
+    date: '2021-11-10',
+    time: '08:00',
+  ),
+];
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,8 +40,24 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Events'),
       ),
-      body: const Center(
-        child: Text('Here will be suggested events'),
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: events.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text(events[index].name),
+                  subtitle: Text(events[index].description),
+                  onTap: () {},
+                ),
+                const Divider(),
+              ],
+            );
+          },
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -48,7 +90,16 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+
+  Future<void> _refreshData() async {
+    await Future.delayed(const Duration(seconds: 1));
+    setState(() {});
   }
 
   void _profileScreen(BuildContext context) {
