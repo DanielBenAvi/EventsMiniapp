9c10edb42d3d69dfe29ef45dba3e0c7811b6be75
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-05-07 09:36:38 +0300

    added app menu

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -11,9 +10,46 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Home'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Events'),
+      ),
+      body: const Center(
+        child: Text('Here will be suggested events'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+            ),
+            ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Logout'),
+                onTap: () {
+                  _loginScreen(context);
+                }),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _loginScreen(BuildContext context) {
+    Navigator.pop(context);
+    Navigator.pushNamed(context, '/login');
   }
 }
